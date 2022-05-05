2fe8275e170e9bab83bc16a0c65abae312d5d4f7
Emmet <104880410+emmetCode@users.noreply.github.com>
2022-05-05 10:55:12 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+const sidebar = body.querySelector(".sidebar");
+const toggle = body.querySelector(".toggle");
+const searchBtn = body.querySelector(".search-box");
+const modeSwtich = body.querySelector(".toggle-switch");
+const modeText = body.querySelector(".mode-text");
+
+modeSwtich.addEventListener("click", () => {
+    body.classList.toggle("dark");
+});
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+});
